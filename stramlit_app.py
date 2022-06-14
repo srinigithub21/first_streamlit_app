25f9c0eb53b86109aa2dafba0d7c8d6395d92865
srinigithub21 <107453435+srinigithub21@users.noreply.github.com>
2022-06-13 23:32:04 -0500

    Update stramlit_app.py

--- stramlit_app.py
+++ stramlit_app.py
@@ -1,3 +1,8 @@
 
 import streamlit
 streamlit.title('My Parents New Healthy Diner')
+streamlit.header('Breakfast Menu')
+streamlit.header('Omega 3 & Blueberry Oatmeal')
+streamlit.header('Kale, Spinach & Rcoket Smoothie')
+streamlit.header('Hard boiled Free Range Egg')
+
